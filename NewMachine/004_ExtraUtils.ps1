322112cb8635cfa2f9fc6095effe7403bdee6e05
danbennett2k8@gmail.com <danbennett2k8@gmail.com>
2023-09-16 00:57:19 +0100

    awdad

--- NewMachine/004_ExtraUtils.ps1
+++ NewMachine/004_ExtraUtils.ps1
@@ -1,2 +1,5 @@
 #Install Revo Uninstaller
 choco install Revo.Uninstaller -y
+
+#Install TeraCopy
+choco install teracopy -y
---
+++ NewMachine/020_Games.ps1
@@ -0,0 +1,8 @@
+#Install Steam
+choco install steam -y
+
+#Install Battle.Net
+choco install battlenet -y
+
+#Install Discord
+choco install discord -y

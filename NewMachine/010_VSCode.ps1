3d17b36b22d6f950e97c070779e9a16958e6203a
danbennett2k8@gmail.com <danbennett2k8@gmail.com>
2023-09-16 00:10:42 +0100

    adadad

--- NewMachine/010_VSCode.ps1
+++ NewMachine/010_VSCode.ps1
@@ -1,6 +1,8 @@
 #Install vscode
 choco install vscode -y
 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 #Install vscode extensions
 code --install-extension ms-vscode.powershell
 code --install-extension github.copilot
